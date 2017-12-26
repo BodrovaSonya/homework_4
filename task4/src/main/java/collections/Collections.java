920fe25240f94cf433e336001d34d658bd578fae
BodrovaSonya <bodrova_sonya@mail.ru>
2017-12-26 17:28:46 +0300

    26/12/17

--- task4/src/main/java/collections/Collections.java
+++ task4/src/main/java/collections/Collections.java
@@ -12,40 +12,69 @@ import java.util.*;
  */
 
 public class Collections {
+    static Scanner scanner;
+    static Map<String, Integer> statistics;
+
+    final static String PATH = "D:\\Документы Бодрова\\Java курс\\ДЗ 4\\homework_4\\task4\\src\\main\\resources\\input.txt";
+
     public static void main(String[] args) throws FileNotFoundException {
-        // чтение слов из файла
-        Scanner scanner = new Scanner(new File("D:\\Документы Бодрова\\Java курс\\ДЗ 4\\homework_4\\task4\\src\\main\\resources\\input.txt"));
-        Map<String, Integer> statistics = new HashMap<String, Integer>();
+        // reading from file
+        readFromFile(PATH);
+
+        // statistics to the console
+        System.out.println(statistics);
+
+        //alphabetical ordering
+        alphabeticalOrdering();
+
+        //output of the most common word
+        mostOften();
+
+    }
+
+    /**
+     * reading from file
+     * @param path to file
+     * @throws FileNotFoundException
+     */
+    static void readFromFile(String path) throws FileNotFoundException {
+        scanner = new Scanner(new File(path));
+        statistics = new HashMap<String, Integer>();
         while (scanner.hasNext()) {
             String word = scanner.useDelimiter("\\s+").next();
-                    Integer count = statistics.get(word);
+            Integer count = statistics.get(word);
             if (count == null) {
                 count = 0;
             }
             statistics.put(word, ++count);
         }
+    }
 
-        // вывод статистики на консоль
-        System.out.println(statistics);
-
-        // дерево для вывода слов в алфавитном порядке
-       /* Set<String> words = new TreeSet<String>();
+    /**
+     * alphabetical ordering using TreeSet
+     */
+   static void alphabeticalOrdering () {
+        Set<String> words = new TreeSet<String>();
         Iterator<Map.Entry<String,Integer>>entryIterator1 = statistics.entrySet().iterator();
         while (entryIterator1.hasNext()){
             words.add(entryIterator1.next().getKey());
         }
-        System.out.println(words);*/
+        System.out.println(words);
+    }
 
-        // поиск самого часто встречающегося слова
+
+    /**
+     * the most common word
+     */
+    static void mostOften () {
         int max = 0;
         String maxKey = null;
         for (String e: statistics.keySet()){
             if (statistics.get(e) > max) {
-               // max = statistics.get(e);
+                max = statistics.get(e);
+                maxKey = e;
             }
         }
-
+        System.out.println("maxKey: " + maxKey + ", maxValue: " + max);
     }
-
-
 }
